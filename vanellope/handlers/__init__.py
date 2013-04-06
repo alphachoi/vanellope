ffe962ddf59e26eb4a908e7920d987174038eac0
qar <qar@outlook.com>
2013-04-06 08:27:40 +0800

    working..

--- vanellope/handlers/__init__.py
+++ vanellope/handlers/__init__.py
@@ -61,5 +61,6 @@ class BaseHandler(tornado.web.RequestHandler):
         except TypeError:
             return {}
 
+        
 
 
--- vanellope/handlers/article.py
+++ vanellope/handlers/article.py
@@ -21,7 +21,7 @@ from vanellope.handlers import BaseHandler
 class ArticleHandler(BaseHandler):
     # URL: /article/ARTICLE_SN
     def get(self, article_sn):
-        article = da.get_article_by_sn(int(article_sn))
+        article = db.article.find_one({"sn": int(article_sn)})
         if not article or article['status'] == cst.DELETED:
             self.send_error(404)
             self.finish()
